ad4ad9f30abef029676c3dd487d1b3df11bf7911
christopher cuzin <ed.ed1009@hotmail.fr>
2022-08-03 16:41:38 +0200

    USRC-54 fixes all bug on objectives part of the research plan

--- assets/js/add_objectives_research_plan.js
+++ assets/js/add_objectives_research_plan.js
@@ -29,15 +29,17 @@ if (document.getElementById('button-add-objectives')) {
             const checkboxCase = document.getElementById('input-checkbox-objectives');
             const checkboxValue = document.getElementById('input-checkbox-text-objectives');
             const divCheckbox = document.getElementById('new-input-checkbox');
+            const label = document.getElementById('newDivLabelDisplay1');
+            
 
             event.preventDefault();
 
             let valueInput = inputCheckbox.value;
             let newDivParagraph = document.createElement("p");
-            newDivParagraph.setAttribute('class', 'all-div-input');
+            
 
             let newInputHidden = document.createElement("input");
-            newInputHidden.setAttribute('type', 'hidden');
+            
             newInputHidden.setAttribute('class', 'all-input-hidden');
             newInputHidden.setAttribute('value', valueInput);
  
@@ -47,12 +49,25 @@ if (document.getElementById('button-add-objectives')) {
             let newCheckbox = document.createElement("input");
             newCheckbox.setAttribute("type", "checkbox");
             newCheckbox.setAttribute("class", "newDivCheck");
+            newCheckbox.setAttribute("disabled", "disabled");
 
             let newLabel = document.createElement("label");
             newLabel.setAttribute("for", "checkbox");
-            newLabel.setAttribute("class", "newDivLabel");
+            
         
             newLabel.innerHTML = valueInput;
+
+            if (label) {
+                newInputHidden.setAttribute('type', 'text');
+                newDivParagraph.setAttribute('class', 'all-div-input-edit');
+                newLabel.setAttribute("class", "newDivLabelDisplay");
+            } else {
+                newInputHidden.setAttribute('type', 'hidden');
+                newDivParagraph.setAttribute('class', 'all-div-input');
+                newLabel.setAttribute("class", "newDivLabel");
+            }
+
+             
 
             divCheckbox.appendChild(newDivParagraph);
 
@@ -61,6 +76,23 @@ if (document.getElementById('button-add-objectives')) {
             newDivParagraph.appendChild(newInputHidden);
 
             inputCheckbox.value = '';
+            const collection = document.querySelectorAll('.all-div-input-edit');
+            let j =1;
+            for (let i = 0; i < collection.length; i++) {
+                const elem = collection[i];
+                newLabel.setAttribute('id', 'newDivLabelDisplay' + j);
+                elem.onclick = function(event) {
+                    let target =  event.target;
+                    if (target === elem) {
+                        target.remove();
+                        const collection = document.querySelectorAll('.all-div-input-edit');
+                        for (const label of collection) {
+                            label.setAttribute('id', 'newDivLabelDisplay' + j);
+                        }
+                    }
+                };
+                j++ 
+            }
         }
     }, true);
 
@@ -70,14 +102,36 @@ if (document.getElementById('button-add-objectives')) {
     
     buttonSaveObjectives.addEventListener('click', () => {
         const allInputHiddenSelected = document.querySelectorAll('.all-input-hidden');
+        const collection = document.querySelectorAll('.all-div-input-edit');
+
+        for (let i = 0; i < collection.length; i++) {
+            const elem = collection[i];
+            elem.classList.remove('all-div-input-edit');
+            elem.classList.add('all-div-input');
+            elem.onclick = function(event) {
+                let target =  event.target;
+                if (target === elem) {
+                    event.preventDefault();
+                }
+            }; 
+        }
 
         let objectives = 1;
         objectivesCount.value = 0;
         allInputHiddenSelected.forEach(input => {
+            const label = document.getElementById('newDivLabelDisplay' + objectives);
             input.setAttribute('name', 'research-plan-objectives-' + objectives);
+            input.setAttribute('type', "hidden");
             objectivesCount.value = objectives;
+            if (label) {
+                label.classList.add('newDivLabel');
+                label.classList.remove('newDivLabelDisplay');
+                label.innerHTML = input.value;
+            }
             objectives++;
         });
+
+        
 
         addObjectivesP.classList.remove('add-objectives-p');
         addObjectivesP.classList.add('add-objectives-p-none');
@@ -111,10 +165,13 @@ if (document.getElementById('button-add-objectives')) {
 
         inputCheckbox.classList.add('input-objectives');
         inputCheckbox.classList.remove('input-objectives-display');
-    
+        
+        let i = 1;
         for (const label of allLabel) {
             label.classList.remove('newDivLabel');
             label.classList.add('newDivLabelDisplay');
+            label.setAttribute('id', 'newDivLabelDisplay' + i);
+            i++
         }
 
         allInputHiddenSelected.forEach(input => {
@@ -122,7 +179,7 @@ if (document.getElementById('button-add-objectives')) {
         });
 
         const collection = document.querySelectorAll('.all-div-input');
-
+        let j =1;
         for (let i = 0; i < collection.length; i++) {
             const elem = collection[i];
             elem.classList.add('all-div-input-edit');
@@ -132,30 +189,19 @@ if (document.getElementById('button-add-objectives')) {
                 if (target === elem) {
                     target.remove();
                 }
-            };  
+                if (target === elem) {
+                    const labels = document.querySelectorAll('.newDivLabelDisplay');
+                    for (const label of labels) {
+                        label.setAttribute('id', 'newDivLabelDisplay' + j);
+                        j++
+                    }
+                }
+                
+            };
+            
         } 
             
         buttonSaveObjectives.classList.remove('button-save-objectives-display');
         buttonSaveObjectives.classList.add('button-save-objectives');
-
-        buttonSaveObjectives.addEventListener('click', () => {
-
-            buttonSaveObjectives.classList.remove('button-save-objectives');
-            buttonSaveObjectives.classList.add('button-save-objectives-display');
-
-            allInputHiddenSelected.forEach(input => {
-                input.setAttribute('class', "all-input-hidden-validate");
-            });
-
-            const collectionEdit = document.querySelectorAll('.all-div-input-edit');
-            for (const elem of collectionEdit) {
-                elem.classList.remove('all-div-input-edit');
-                elem.classList.add('all-div-input');
-            } 
-
-            sendResearchPlanButton.classList.remove('send-research-plan-disabled');
-            sendResearchPlanButton.classList.add('send-research-plan');
-            sendResearchPlanButton.removeAttribute('disabled');
-        });
     });
 }

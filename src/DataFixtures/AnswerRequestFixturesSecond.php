cbf3616ef8123726a7814cd80809249781f9b5e0
christopher cuzin <ed.ed1009@hotmail.fr>
2022-08-08 14:28:27 +0200

    USRC-63 change status request to solved when a plan is available

--- src/DataFixtures/AnswerRequestFixturesSecond.php
+++ src/DataFixtures/AnswerRequestFixturesSecond.php
@@ -9,66 +9,8 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
-class AnswerRequestFixtures extends Fixture implements DependentFixtureInterface
+class AnswerRequestFixturesSecond extends Fixture implements DependentFixtureInterface
 {
-    private const ANSWER_REQUEST_FIRST_PART = [
-        [
-            'name' => 'section',
-            'question' => 'Requestor',
-            'answer' => 'Requestor'
-        ],
-        [
-            'name' => 'open-question',
-            'question' => 'Project name',
-            'answer' => 'Wild Project'
-        ],
-        [
-            'name' => 'section',
-            'question' => 'Research objectives',
-            'answer' => 'Research objectives'
-        ],
-        [
-            'name' => 'open-question',
-            'question' => 'Research goal',
-            'answer' => 'the goal of Lean is to deliver the maximum customer
-                            value in the shortest sustainable lead time while
-                            providing the highest possible quality to customers and society.'
-        ],
-        [
-            'name' => 'open-question',
-            'question' => 'Available data',
-            'answer' => 'Cover almost all user-related topics, gather informations on
-                            users feelings, motivations and daily routines, or how they use products.'
-        ],
-        [
-            'name' => 'open-question',
-            'question' => 'Research questions',
-            'answer' => 'Cover almost all user-related topics, gather informations on
-                            users feelings, motivations and daily routines, or how they use products.'
-        ],
-        [
-            'name' => 'separator',
-            'question' => 'separator',
-            'answer' => 'separator'
-        ],
-        [
-            'name' => 'section',
-            'question' => 'Notes & additional comments',
-            'answer' => 'Notes & additional comments'
-        ],
-        [
-            'name' => 'date-picker',
-            'question' => 'What is the deadline of your research?',
-            'answer' => '2022-07-15'
-        ],
-        [
-            'name' => 'open-question',
-            'question' => 'Note from requestor',
-            'answer' => 'No answer'
-        ]
-
-    ];
-
     private const ANSWER_REQUEST_SECOND_PART = [
         [
             'name' => 'section',
@@ -120,31 +62,16 @@ class AnswerRequestFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
-        for ($count = 4; $count <= 6; $count++) {
-            foreach (self::ANSWER_REQUEST_FIRST_PART as $answerRequestValue) {
-                $answerRequest = new AnswerRequest();
-                $answerRequest
-                    ->setName($answerRequestValue['name'])
-                    ->setQuestion($answerRequestValue['question'])
-                    ->setAnswer($answerRequestValue['answer']);
-                if ($this->getReference('research_request_' . $count) instanceof ResearchRequest) {
-                    $answerRequest->setResearchRequest($this->getReference('research_request_' . $count));
-                }
-
-                $manager->persist($answerRequest);
-            }
-        }
-
         for ($count = 1; $count <= 3; $count++) {
             foreach (self::ANSWER_REQUEST_SECOND_PART as $answerRequestValue) {
                 $answerRequest = new AnswerRequest();
-                if ($answerRequestValue['name'] != 'multiple-choice') {
+                if ($answerRequestValue['name'] != 'multiple-choice' && is_string($answerRequestValue['answer'])) {
                     $answerRequest
                         ->setName($answerRequestValue['name'])
                         ->setQuestion($answerRequestValue['question'])
                         ->setAnswer($answerRequestValue['answer'])
                         ;
-                } else {
+                } elseif (is_array($answerRequestValue['answer'])) {
                     $answerRequest
                         ->setName($answerRequestValue['name'])
                         ->setQuestion($answerRequestValue['question'])
@@ -152,15 +79,15 @@ class AnswerRequestFixtures extends Fixture implements DependentFixtureInterface
                         ->setMultipleAnswers($answerRequestValue['answer'])
                         ;
                 }
+
                 if ($this->getReference('research_request_' . $count) instanceof ResearchRequest) {
                     $answerRequest->setResearchRequest($this->getReference('research_request_' . $count));
                 }
 
                 $manager->persist($answerRequest);
             }
+            $manager->flush();
         }
-
-        $manager->flush();
     }
 
     public function getDependencies()
